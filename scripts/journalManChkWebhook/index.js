afdfd3b46b0fc06d38c9b312031959ff1f487498
Derrick Mehaffy <derrickmehaffy@gmail.com>
2019-10-21 02:20:44 -0700

    finish up journalChecker

--- scripts/journalManChkWebhook/index.js
+++ scripts/journalManChkWebhook/index.js
@@ -1,25 +1,98 @@
 'use strict';
 
+const cron = require('node-cron');
 const fetch = require('fetch-retry');
-const webhook = require('webhook-discord');
 const fs = require('fs');
+const hookcord = require('hookcord');
 const path = require('path');
+require('dotenv').config({ path: require('find-config')('.env') });
 
-// Check if latest file exists else create it from example
 const latestPath = path.join(__dirname, 'data', 'latest.json');
 const examplePath = path.join(__dirname, 'data', 'example.json');
 let latestVersion = {};
 
 try {
-  if (fs.existsSync(latestPath)) {
-    let rawFile = fs.readFileSync(latestPath);
-    latestVersion = JSON.parse(rawFile);
-  } else {
-    let exampleRawFile = fs.readFileSync(examplePath);
-    fs.writeFileSync(latestPath, exampleRawFile);
-    latestVersion = JSON.parse(exampleRawFile);
-  }
-} catch(err) {
-  console.log(err)
+	if (fs.existsSync(latestPath)) {
+		let rawFile = fs.readFileSync(latestPath);
+		latestVersion = JSON.parse(rawFile);
+	} else {
+		let exampleRawFile = fs.readFileSync(examplePath);
+		fs.writeFileSync(latestPath, exampleRawFile);
+		latestVersion = JSON.parse(exampleRawFile);
+	}
+} catch (err) {
+	console.log(err);
+}
+
+const downloadJournal = async version => {
+	let journalPath = path.join(__dirname, 'data', `Journal-Manual-v${version}.pdf`);
+	let journalURL = process.env.FDEV_JNL_URL + `/v${version}/Journal-Manual-v${version}.pdf`;
+
+	const res = await fetch(journalURL);
+	if (res.status === 200) {
+		console.log(`New Version v${version} is available, downloading...`);
+		const fileStream = fs.createWriteStream(journalPath);
+		await new Promise((resolve, reject) => {
+			res.body.pipe(fileStream);
+			res.body.on('error', err => {
+				console.log('An error occured downloading the new version');
+				console.log('== End FDev Journal Checker ==');
+				reject(err);
+			});
+			fileStream.on('finish', function() {
+				console.log(`New Journal v${version} saved to data directory`);
+
+				// Update latest.json with new version number
+				latestVersion.journalVersion = latestVersion.journalVersion + 1;
+				fs.writeFileSync(latestPath, JSON.stringify(latestVersion));
+				console.log(`Updated latest file with v${version + 1}`);
+
+				// Fire Discord Webhook Alerts
+				if (process.env.WEBHOOK_RD !== '') {
+					new hookcord.Hook()
+						.setLink(process.env.WEBHOOK_RD)
+						.setPayload({
+							content: `<@${process.env.USER_DMID}>/<@${process.env.USER_LCUID}> New Journal Found at: ${journalURL}`,
+						})
+						.fire()
+						.then(function(response) {
+							console.log('Fired webhook to Canonn R&D channel');
+						})
+						.catch(function(e) {
+							console.log(e);
+						});
+				}
+
+				if (process.env.WEBHOOK_KZ !== '') {
+					new hookcord.Hook()
+						.setLink(process.env.WEBHOOK_KZ)
+						.setPayload({
+							content: `<@${process.env.USER_KZID}> New Journal Found at: ${journalURL}`,
+						})
+						.fire()
+						.then(function(response) {
+							console.log('Fired webhook to Kazakov');
+						})
+						.catch(function(e) {
+							console.log(e);
+						});
+				}
+
+				console.log('== End FDev Journal Checker ==');
+				resolve();
+			});
+		});
+	} else {
+		console.log('New Journal version is not available, current is v' + (version - 1));
+    console.log('== End FDev Journal Checker ==');
+	}
 };
 
+if (latestVersion) {
+  cron.schedule(process.env.CRON_JNL, () => {
+    console.log('== Starting FDev Journal Checker ==');
+    downloadJournal(latestVersion.journalVersion);
+  });
+} else {
+  console.log('Error No latest version file');
+}
