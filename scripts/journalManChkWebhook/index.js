f250e0f441520df54c6fcaacafd72fbc20080d55
Derrick Mehaffy <derrickmehaffy@gmail.com>
2019-10-21 00:37:09 -0700

    More structure, will also save a local copy of the journal eventually

--- scripts/journalManChkWebhook/index.js
+++ scripts/journalManChkWebhook/index.js
@@ -1,3 +1,25 @@
+'use strict';
+
 const fetch = require('fetch-retry');
 const webhook = require('webhook-discord');
+const fs = require('fs');
+const path = require('path');
+
+// Check if latest file exists else create it from example
+const latestPath = path.join(__dirname, 'data', 'latest.json');
+const examplePath = path.join(__dirname, 'data', 'example.json');
+let latestVersion = {};
+
+try {
+  if (fs.existsSync(latestPath)) {
+    let rawFile = fs.readFileSync(latestPath);
+    latestVersion = JSON.parse(rawFile);
+  } else {
+    let exampleRawFile = fs.readFileSync(examplePath);
+    fs.writeFileSync(latestPath, exampleRawFile);
+    latestVersion = JSON.parse(exampleRawFile);
+  }
+} catch(err) {
+  console.log(err)
+};
 
