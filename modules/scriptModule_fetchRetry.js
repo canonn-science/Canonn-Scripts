170722f3fb0cf7d103a16231845ea4700acc65aa
Derrick Mehaffy <derrickmehaffy@gmail.com>
2020-07-06 03:28:29 -0700

    move to Axios retry with bigint support

--- modules/scriptModule_fetchRetry.js
+++ modules/scriptModule_fetchRetry.js
@@ -1,32 +1,50 @@
-var fetch = require('fetch-retry');
+const rax = require('retry-axios');
+const axios = require('axios');
+const JSONbig = require('json-bigint')({"storeAsString": true});
 let logger = require('perfect-logger');
 let settings = require('../settings.json');
 
 module.exports = {
 	fetchRetry: async (url, retryCount = settings.global.retryCount, delay = settings.global.delay * 100, options) => {
-		options.retries = retryCount;
-		options.retryDelay = delay * 300;
-		options.retryOn = (attempt, error, response) => {
-			if (attempt >= retryCount) {
-				response = {};
-				logger.warn('Request failed, skipping');
-				return false;
+		const interceptorId = rax.attach();
+		const res = await axios({
+			method: options.method,
+			headers: options.headers,
+			data: options.body,
+			url,
+			transformResponse: [data  => data],
+			raxConfig: {
+				// Retry 3 times on requests that return a response (500, etc) before giving up.  Defaults to 3.
+				retry: retryCount,
+	
+				// Retry twice on errors that don't return a response (ENOTFOUND, ETIMEDOUT, etc).
+				noResponseRetries: 2,
+	
+				// Milliseconds to delay at first.  Defaults to 100.
+				retryDelay: delay,
+	
+				// HTTP methods to automatically retry.  Defaults to:
+				// ['GET', 'HEAD', 'OPTIONS', 'DELETE', 'PUT']
+				httpMethodsToRetry: ['GET', 'HEAD', 'OPTIONS', 'DELETE', 'PUT'],
+	
+				// The response status codes to retry.  Supports a double
+				// array with a list of ranges.  Defaults to:
+				// [[100, 199], [429, 429], [500, 599]]
+				statusCodesToRetry: [[100, 199], [429, 429], [500, 599]],
+	
+				// You can set the backoff type.
+				// options are 'exponential' (default), 'static' or 'linear'
+				backoffType: 'exponential',
+	
+				// You can detect when a retry is happening, and figure out how many
+				// retry attempts have been made
+				onRetryAttempt: err => {
+					const cfg = rax.getConfig(err);
+					logger.crit(`Retry attempt #${cfg.currentRetryAttempt}`);
+				}
 			}
-			if (response && response.status && response.status === 429) {
-				logger.warn(
-					`Rate limit Exceeded, retrying in: ${options.retryDelay}ms Attempt number: ${attempt + 1}`
-				);
-				return true;
-			}
-			if (error !== null || response.status >= 400) {
-				logger.crit(`Request Error, retrying in: ${options.retryDelay}ms Attempt number: ${attempt + 1}`);
-				console.log(response);
-				return true;
-			}
-		};
+		});
 
-		let data = await fetch(url, options);
-
-		return data.json();
+		return data = JSONbig.parse(res.data);
 	},
 };
