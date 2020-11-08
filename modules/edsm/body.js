66f0ddb063c5eb0134201ff31bfa3b0f3f7862ab
Derrick Mehaffy <derrickmehaffy@gmail.com>
2020-11-07 19:40:26 -0700

    Split EDSM into sub-modules

---
+++ modules/edsm/body.js
@@ -0,0 +1,22 @@
+const { fetchRetry } = require('../utils');
+
+// Fetch EDSM Body
+async function getBody(system) {
+	let edsmBodyURL = `https://www.edsm.net/en/api-system-v1/bodies?systemName=${encodeURIComponent(system)}`;
+
+	let edsmBodyData = {};
+	try {
+		edsmBodyData = await fetchRetry(edsmBodyURL, 5, 5000, {
+			method: 'GET',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+		});
+	} catch (error) {
+		console.log(error);
+	}
+	return await edsmBodyData;
+}
+
+module.exports = getBody;
